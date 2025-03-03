e3e87efafb2a610da1fe00054b2a29ef465465da
albc9 <abordach108@ub.edu.cat>
2025-03-03 09:21:19 +0100

    cositas

--- src/prog2/model/Bungalow.java
+++ src/prog2/model/Bungalow.java
@@ -2,8 +2,49 @@ package prog2.model;
 
 public class Bungalow extends Casa {
 
-    public Bungalow() {
+
+    private int placesParquing;
+    private boolean terrassa;
+    private boolean tv;
+    private boolean aireFred;
+
+
+    //CONSTRUCTOR
+    public Bungalow(String nom, String idAllotjament, Mida mida, int habitacions, int placesPersones, int placesParquing, boolean terrassa, boolean tv, boolean aireFred) {
+        super(Casa.Mida.MITJANA, habitacions, placesPersones );
         super.setEstadaMinima(7,4);
-        super.setNom("Bungalow");
+        super.setId(idAllotjament);
+        super.setNom(nom);
+        setPlacesParquing(placesParquing);
+        setTerrassa(terrassa);
+        setTv(tv);
+        setAireFred(aireFred);
     }
+    // Getter y Setter
+    public int getPlacesParquing() {
+        return placesParquing;
+    }
+    public void setPlacesParquing(int placesParquing) {
+        this.placesParquing = placesParquing;
+    }
+    public boolean isTerrassa() {
+        return terrassa;
+    }
+    public void setTerrassa(boolean terrassa) {
+        this.terrassa = terrassa;
+    }
+    public boolean isTv() {
+        return tv;
+    }
+    public void setTv(boolean tv) {
+        this.tv = tv;
+    }
+    public boolean isAireFred() {
+        return aireFred;
+    }
+    public void setAireFred(boolean aireFred) {
+        this.aireFred = aireFred;
+    }
+
+
 }
--- src/prog2/model/Glamping.java
+++ src/prog2/model/Glamping.java
@@ -1,4 +1,32 @@
 package prog2.model;
 
 public class Glamping extends Casa {
+
+    private String material = "Tela";
+    private boolean casaMascota = true;
+
+    //CONSTRUCTOR
+    public Glamping(String nom, String idAllotjament, Mida mida, int habitacions, int placesPersones, int placesParquing, boolean terrassa, boolean tv, boolean aireFred) {
+        super(Casa.Mida.MITJANA, habitacions, placesPersones );
+        super.setEstadaMinima(7,4);
+        super.setId(idAllotjament);
+        super.setNom(nom);
+        setMaterial(material);
+        setCasaMascota(casaMascota);
+
+    }
+    // Getter y Setter
+    public String getMaterial() {
+        return material;
+    }
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+    public boolean isCasaMascota() {
+        return casaMascota;
+    }
+    public void setCasaMascota(boolean casaMascota) {
+        this.casaMascota = casaMascota;
+    }
+
 }
